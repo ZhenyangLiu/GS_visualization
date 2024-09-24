2b6d1792dca6f20a6353a29fce2c3abd566b524e
Zhenyang Liu <lzyzjhz@163.com>
2024-09-24 10:32:32 +0800

    GS_vis

---
+++ visiualization_gs.py
@@ -0,0 +1,451 @@
+# Borrowed from OmniSeg3D-GS (https://github.com/OceanYing/OmniSeg3D-GS)
+import torch
+from scene import Scene
+import os
+from tqdm import tqdm
+from os import makedirs
+from gaussian_renderer import render
+import torchvision
+from utils.general_utils import safe_state
+from argparse import ArgumentParser
+from arguments import ModelParams, PipelineParams, get_combined_args
+# from gaussian_renderer import GaussianModel
+import numpy as np
+from PIL import Image
+import colorsys
+import cv2
+from sklearn.decomposition import PCA
+
+# from scene.gaussian_model import GaussianModel
+from scene import Scene, GaussianModel
+import dearpygui.dearpygui as dpg
+import math
+from scene.cameras import Camera
+from utils.graphics_utils import getWorld2View2, focal2fov, fov2focal
+
+from scipy.spatial.transform import Rotation as R
+
+def depth2img(depth):
+    depth = (depth-depth.min())/(depth.max()-depth.min() + 1e-7)
+    depth_img = cv2.applyColorMap((depth*255).astype(np.uint8),
+                                  cv2.COLORMAP_TURBO)
+    return depth_img
+
+class CONFIG:
+    r = 2   # scale ratio
+    window_width = int(2160/r)
+    window_height = int(1200/r)
+
+    width = int(2160/r)
+    height = int(1200/r)
+
+    radius = 2
+
+    debug = False
+    dt_gamma = 0.2
+
+    # gaussian model
+    sh_degree = 3
+    MODEL_PATH = './output/horns' # 30000
+
+
+    convert_SHs_python = False
+    compute_cov3D_python = False
+
+    white_background = False
+
+    SCENE_GAUSSIAN_ITERATION = 30000
+
+    SCENE_PCD_PATH = os.path.join(MODEL_PATH, f'point_cloud/iteration_{str(SCENE_GAUSSIAN_ITERATION)}/scene_point_cloud.ply')
+
+
+class OrbitCamera:
+    def __init__(self, W, H, r=2, fovy=60):
+        self.W = W
+        self.H = H
+        self.radius = r  # camera distance from center
+        self.center = np.array([0, 0, 0], dtype=np.float32)  # look at this point
+        self.rot = R.from_quat(
+            [0, 0, 0, 1]
+        )  # init camera matrix: [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+        self.up = np.array([0, 1, 0], dtype=np.float32)  # need to be normalized!
+        self.right = np.array([1, 0, 0], dtype=np.float32)  # need to be normalized!
+        self.fovy = fovy
+        self.translate = np.array([0, 0, self.radius])
+        self.scale_f = 1.0
+
+
+        self.rot_mode = 1   # rotation mode (1: self.pose_movecenter (movable rotation center), 0: self.pose_objcenter (fixed scene center))
+        # self.rot_mode = 0
+
+
+    @property
+    def pose_movecenter(self):
+        # --- first move camera to radius : in world coordinate--- #
+        res = np.eye(4, dtype=np.float32)
+        res[2, 3] -= self.radius
+        
+        # --- rotate: Rc --- #
+        rot = np.eye(4, dtype=np.float32)
+        rot[:3, :3] = self.rot.as_matrix()
+        res = rot @ res
+
+        # --- translate: tc --- #
+        res[:3, 3] -= self.center
+        
+        # --- Convention Transform --- #
+        # now we have got matrix res=c2w=[Rc|tc], but gaussian-splatting requires convention as [Rc|-Rc.T@tc]
+        res[:3, 3] = -rot[:3, :3].transpose() @ res[:3, 3]
+        
+        return res
+    
+    @property
+    def pose_objcenter(self):
+        res = np.eye(4, dtype=np.float32)
+        
+        # --- rotate: Rw --- #
+        rot = np.eye(4, dtype=np.float32)
+        rot[:3, :3] = self.rot.as_matrix()
+        res = rot @ res
+
+        # --- translate: tw --- #
+        res[2, 3] += self.radius    # camera coordinate z-axis
+        res[:3, 3] -= self.center   # camera coordinate x,y-axis
+        
+        # --- Convention Transform --- #
+        # now we have got matrix res=w2c=[Rw|tw], but gaussian-splatting requires convention as [Rc|-Rc.T@tc]=[Rw.T|tw]
+        res[:3, :3] = rot[:3, :3].transpose()
+        
+        return res
+
+    @property
+    def opt_pose(self):
+        # --- deprecated ! Not intuitive implementation --- #
+        res = np.eye(4, dtype=np.float32)
+
+        res[:3, :3] = self.rot.as_matrix()
+
+        scale_mat = np.eye(4)
+        scale_mat[0, 0] = self.scale_f      # why apply scale ratio to rotation matrix? It's confusing.
+        scale_mat[1, 1] = self.scale_f
+        scale_mat[2, 2] = self.scale_f
+
+        transl = self.translate - self.center
+        transl_mat = np.eye(4)
+        transl_mat[:3, 3] = transl
+
+        return transl_mat @ scale_mat @ res
+
+    # intrinsics
+    @property
+    def intrinsics(self):
+        focal = self.H / (2 * np.tan(np.radians(self.fovy) / 2))
+        return np.array([focal, focal, self.W // 2, self.H // 2])
+
+    def orbit(self, dx, dy):
+        if self.rot_mode == 1:    # rotate the camera axis, in world coordinate system
+            up = self.rot.as_matrix()[:3, 1]
+            side = self.rot.as_matrix()[:3, 0]
+        elif self.rot_mode == 0:    # rotate in camera coordinate system
+            up = -self.up
+            side = -self.right
+        rotvec_x = up * np.radians(0.01 * dx)
+        rotvec_y = side * np.radians(0.01 * dy)
+
+        self.rot = R.from_rotvec(rotvec_x) * R.from_rotvec(rotvec_y) * self.rot
+
+    def scale(self, delta):
+        # self.radius *= 1.1 ** (-delta)    # non-linear version
+        self.radius -= 0.1 * delta      # linear version
+
+    def pan(self, dx, dy, dz=0):
+        
+        if self.rot_mode == 1:
+            # pan in camera coordinate system: project from [Coord_c] to [Coord_w]
+            self.center += 0.0005 * self.rot.as_matrix()[:3, :3] @ np.array([dx, -dy, dz])
+        elif self.rot_mode == 0:
+            # pan in world coordinate system: at [Coord_w]
+            self.center += 0.0005 * np.array([-dx, dy, dz])
+
+
+class GaussianSplattingGUI:
+    def __init__(self, opt, gaussian_model:GaussianModel) -> None:
+        self.opt = opt
+
+        self.width = opt.width
+        self.height = opt.height
+        self.window_width = opt.window_width
+        self.window_height = opt.window_height
+        self.camera = OrbitCamera(opt.width, opt.height, r=opt.radius)
+
+        bg_color = [1, 1, 1] if opt.white_background else [0, 0, 0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+
+        self.bg_color = background
+        self.render_buffer = np.zeros((self.width, self.height, 3), dtype=np.float32)
+        self.update_camera = True
+        self.dynamic_resolution = True
+        self.debug = opt.debug
+        self.engine = {
+            'scene': gaussian_model,
+        }
+
+        self.load_model = False
+        print("loading model file...")
+        self.engine['scene'].load_ply(self.opt.SCENE_PCD_PATH)
+        self.load_model = True
+        print("loading model file done.")
+
+        self.mode = "image"  # choose from ['image', 'depth']
+
+        dpg.create_context()
+        self.register_dpg()
+
+        self.frame_id = 0
+
+        # --- for better operation --- #
+        self.clickmode_button = False
+        self.clickmode_multi_button = False
+        self.moving = False
+        self.moving_middle = False
+        self.mouse_pos = (0, 0)
+
+        self.render_mode_rgb = False
+
+        self.save_flag = False
+    def __del__(self):
+        dpg.destroy_context()
+
+    def prepare_buffer(self, outputs):
+        if self.model == "images":
+            return outputs["render"]
+        else:
+            return np.expand_dims(outputs["depth"], -1).repeat(3, -1)
+    
+    def register_dpg(self):
+        
+        ### register texture
+        with dpg.texture_registry(show=False):
+            dpg.add_raw_texture(self.width, self.height, self.render_buffer, format=dpg.mvFormat_Float_rgb, tag="_texture")
+
+        ### register window
+        with dpg.window(tag="_primary_window", width=self.window_width+300, height=self.window_height):
+            dpg.add_image("_texture")   # add the texture
+
+        dpg.set_primary_window("_primary_window", True)
+        
+        def clickmode_callback(sender):
+            self.clickmode_button = 1 - self.clickmode_button
+        def clickmode_multi_callback(sender):
+            self.clickmode_multi_button = dpg.get_value(sender)
+        # def callback_depth(sender, app_data):
+            # self.img_mode = (self.img_mode + 1) % 4
+            
+        # --- interactive mode switch --- #
+        def callback_save():
+            self.save_flag = True
+        def callback_reload():
+            self.reload_flag = True
+        def render_mode_rgb_callback(sender):
+            self.render_mode_rgb = not self.render_mode_rgb
+        # control window
+        with dpg.window(label="Control", tag="_control_window", width=300, height=550, pos=[self.window_width+10, 0]):
+
+            dpg.add_text("Mouse position: click anywhere to start. ", tag="pos_item")
+            dpg.add_checkbox(label="clickmode", callback=clickmode_callback, user_data="Some Data")
+            dpg.add_checkbox(label="multi-clickmode", callback=clickmode_multi_callback, user_data="Some Data")
+
+            dpg.add_text("\nRender option: ", tag="render")
+            dpg.add_checkbox(label="RGB", callback=render_mode_rgb_callback, user_data="Some Data")
+            dpg.add_button(label="reload_data", callback=callback_reload, user_data="Some Data")
+
+            def callback(sender, app_data, user_data):
+                self.load_model = False
+                file_data = app_data["selections"]
+                file_names = []
+                for key in file_data.keys():
+                    file_names.append(key)
+
+                self.opt.ply_file = file_data[file_names[0]]
+
+                # if not self.load_model:
+                print("loading model file...")
+                self.engine.load_ply(self.opt.ply_file)
+                self.do_pca()   # calculate new self.proj_mat after loading new .ply file
+                print("loading model file done.")
+                self.load_model = True
+
+        if self.debug:
+            with dpg.collapsing_header(label="Debug"):
+                dpg.add_separator()
+                dpg.add_text("Camera Pose:")
+                dpg.add_text(str(self.camera.pose), tag="_log_pose")
+
+
+        def callback_camera_wheel_scale(sender, app_data):
+            if not dpg.is_item_focused("_primary_window"):
+                return
+            delta = app_data
+            self.camera.scale(delta)
+            self.update_camera = True
+            if self.debug:
+                dpg.set_value("_log_pose", str(self.camera.pose))
+        
+
+        def toggle_moving_left():
+            self.moving = not self.moving
+
+
+        def toggle_moving_middle():
+            self.moving_middle = not self.moving_middle
+
+
+        def move_handler(sender, pos, user):
+            if self.moving and dpg.is_item_focused("_primary_window"):
+                dx = self.mouse_pos[0] - pos[0]
+                dy = self.mouse_pos[1] - pos[1]
+                if dx != 0.0 or dy != 0.0:
+                    self.camera.orbit(-dx*30, dy*30)
+                    self.update_camera = True
+
+            if self.moving_middle and dpg.is_item_focused("_primary_window"):
+                dx = self.mouse_pos[0] - pos[0]
+                dy = self.mouse_pos[1] - pos[1]
+                if dx != 0.0 or dy != 0.0:
+                    self.camera.pan(-dx*20, dy*20)
+                    self.update_camera = True
+            
+            self.mouse_pos = pos
+
+
+        def change_pos(sender, app_data):
+            # if not dpg.is_item_focused("_primary_window"):
+            #     return
+            xy = dpg.get_mouse_pos(local=False)
+            dpg.set_value("pos_item", f"Mouse position = ({xy[0]}, {xy[1]})")
+            if self.clickmode_button and app_data == 1:     # in the click mode and right click
+                print(xy)
+                self.new_click_xy = np.array(xy)
+                self.new_click = True
+
+        def key_press_handler(sender, app_data, user_data):
+            # 使用 W A S D 键控制相机的上下左右移动
+            if app_data == dpg.mvKey_W:
+                self.camera.pan(0, -50)  # 向上
+            elif app_data == dpg.mvKey_S:
+                self.camera.pan(0, 50)  # 向下
+            elif app_data == dpg.mvKey_A:
+                self.camera.pan(-50, 0)  # 向左
+            elif app_data == dpg.mvKey_D:
+                self.camera.pan(50, 0)  # 向右
+            
+            self.update_camera = True
+
+        with dpg.handler_registry():
+            dpg.add_mouse_wheel_handler(callback=callback_camera_wheel_scale)
+            
+            dpg.add_mouse_click_handler(dpg.mvMouseButton_Left, callback=lambda:toggle_moving_left())
+            dpg.add_mouse_release_handler(dpg.mvMouseButton_Left, callback=lambda:toggle_moving_left())
+            dpg.add_mouse_click_handler(dpg.mvMouseButton_Middle, callback=lambda:toggle_moving_middle())
+            dpg.add_mouse_release_handler(dpg.mvMouseButton_Middle, callback=lambda:toggle_moving_middle())
+            dpg.add_mouse_move_handler(callback=lambda s, a, u:move_handler(s, a, u))
+            
+            dpg.add_mouse_click_handler(callback=change_pos)
+            dpg.add_key_press_handler(callback=key_press_handler)  # 键盘事件处理
+
+        dpg.create_viewport(title="Gaussian-Splatting-Viewer", width=self.window_width+320, height=self.window_height, resizable=False)
+
+        ### global theme
+        with dpg.theme() as theme_no_padding:
+            with dpg.theme_component(dpg.mvAll):
+                # set all padding to 0 to avoid scroll bar
+                dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 0, 0, category=dpg.mvThemeCat_Core)
+                dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 0, 0, category=dpg.mvThemeCat_Core)
+                dpg.add_theme_style(dpg.mvStyleVar_CellPadding, 0, 0, category=dpg.mvThemeCat_Core)
+        dpg.bind_item_theme("_primary_window", theme_no_padding)
+
+        dpg.setup_dearpygui()
+
+        dpg.show_viewport()
+
+
+    def render(self):
+        while dpg.is_dearpygui_running():
+            # update texture every frame
+            # TODO : fetch rgb and depth
+            if self.load_model:
+                cam = self.construct_camera()
+                self.fetch_data(cam)
+            dpg.render_dearpygui_frame()
+
+
+    def construct_camera(
+        self,
+    ) -> Camera:
+        if self.camera.rot_mode == 1:
+            pose = self.camera.pose_movecenter
+        elif self.camera.rot_mode == 0:
+            pose = self.camera.pose_objcenter
+
+        R = pose[:3, :3]
+        t = pose[:3, 3]
+
+        ss = math.pi / 180.0
+        fovy = self.camera.fovy * ss
+
+        fy = fov2focal(fovy, self.height)
+        fovx = focal2fov(fy, self.width)
+
+        cam = Camera(
+            colmap_id=0,
+            R=R,
+            T=t,
+            FoVx=fovx,
+            FoVy=fovy,
+            image=torch.zeros([3, self.height, self.width]),
+            gt_alpha_mask=None,
+            image_name=None,
+            uid=0,
+        )
+        cam.feature_height, cam.feature_width = self.height, self.width
+        return cam
+
+
+    @torch.no_grad()
+    def fetch_data(self, view_camera):
+        scene_outputs = render(view_camera, self.engine['scene'], self.opt, self.bg_color)
+        
+        img = scene_outputs["render"].permute(1, 2, 0)  #
+        rgb_score = img.clone()
+
+        self.render_buffer = None
+        render_num = 0
+        if self.render_mode_rgb:
+            self.render_buffer = rgb_score.cpu().numpy().reshape(-1)
+            render_num += 1
+        
+        # self.render_buffer /= render_num
+
+        dpg.set_value("_texture", self.render_buffer)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="GUI option")
+
+    parser.add_argument('-m', '--model_path', type=str, default="./output/figurines")
+    parser.add_argument('-s', '--scene_iteration', type=int, default=30000)
+
+    args = parser.parse_args()
+
+    opt = CONFIG()
+
+    opt.MODEL_PATH = args.model_path
+    opt.SCENE_GAUSSIAN_ITERATION = args.scene_iteration
+    opt.SCENE_PCD_PATH = os.path.join(opt.MODEL_PATH)
+
+    gs_model = GaussianModel(opt.sh_degree)
+
+    gui = GaussianSplattingGUI(opt, gs_model)
+
+    gui.render()
